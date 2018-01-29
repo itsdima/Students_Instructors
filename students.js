de39b213c7a29b6e6678a4d93b059131b96e43d6
Eric Pricop <dimitripricop@yahoo.com>
2018-01-28 21:59:09 -0800

    everything but the numbers done

--- students.js
+++ students.js
@@ -6,8 +6,33 @@ function Students() {
      	{first_name : 'KB', last_name : 'Tonel'}
 	]
 	for (var i = 0; i<students.length; i++){
-		console.log(students[i].first_name, students[i].last_name); 
+		console.log(i+1 + '-', students[i].first_name, students[i].last_name); 
 	}
 }
 
 Students(); 
+//--------------------------------
+function Students_instructors(){
+	var users = {
+ 	'Students': [ 
+    	 {first_name:  'Michael', last_name : 'Jordan'},
+     	{first_name : 'John', last_name : 'Rosales'},
+     	{first_name : 'Mark', last_name : 'Guillen'},
+     	{first_name : 'KB', last_name : 'Tonel'}
+  	],
+ 	'Instructors': [
+     	{first_name : 'Michael', last_name : 'Choi'},
+     	{first_name : 'Martin', last_name : 'Puryear'}
+  	]
+	} 
+ 	console.log('Students'); 
+ 	for (var i = 0; i< users.Students.length; i++){
+   		console.log(i+1+' ' +'-',users.Students[i].first_name, users.Students[i].last_name,'-');
+ 	}
+ 	console.log('Instructors'); 
+ 	for (var x = 0; x< users.Instructors.length; x++){
+   		console.log(x+1 +' ' + '-',users.Instructors[x].first_name, users.Instructors[x].last_name, '-');
+ 	}
+}
+ 
+Students_instructors();
